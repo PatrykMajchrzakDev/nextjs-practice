daa5e8a8c093beaff55426653f539d58f12bc4d9
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-09-28 18:06:36 +0200

    Changed dummy image to gmail profile image src

--- promptify/components/Navbar.jsx
+++ promptify/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -71,7 +71,7 @@ const Navbar = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
