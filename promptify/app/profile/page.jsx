a79d7627a45e8cf743f08b8f1e1f8885215bbd28
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-10-07 13:07:10 +0200

    Added edit and delete logic to buttons to delete or edit prompts

--- promptify/app/profile/page.jsx
+++ promptify/app/profile/page.jsx
@@ -1,26 +1,45 @@
 "use client";
-import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
 import Profile from "@/components/Profile";
+import { useDataFetching } from "@/hooks/useDataFetching";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
-  const [posts, setPosts] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
-
-  const fetchPosts = async () => {
-    const response = await fetch(`/api/users/${session?.user.id}/posts`);
-    const data = await response.json();
-    setPosts(data);
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  useEffect(() => {
-    if (session?.user.id) fetchPosts();
-  }, []);
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        // Use the fetchData function from the useDataFetching hook to update the data
+        fetchData();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const postsUrl = session?.user.id
+    ? `/api/users/${session.user.id}/posts`
+    : null;
+
+  const { data: posts, isLoading, fetchData } = useDataFetching(postsUrl, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Profile
--- promptify/components/PromptCard.jsx
+++ promptify/components/PromptCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick }) => {
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
@@ -60,13 +60,13 @@ const PromptCard = ({ post, handleTagClick }) => {
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer"
-            // onClick={handleEdit}
+            onClick={handleEdit}
           >
             Edit
           </p>
           <p
             className="font-inter text-sm orange_gradient cursor-pointer"
-            // onClick={handleDelete}
+            onClick={handleDelete}
           >
             Delete
           </p>
