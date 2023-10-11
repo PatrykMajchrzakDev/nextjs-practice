bc6f6c4509f184c92a80e36e51415722bbcad35c
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-10-11 19:09:16 +0200

    Commented debouncing function to make filtering on the client side instead of new fetch request and updated search bar to find users and tags

--- promptify/app/api/prompt/route.js
+++ promptify/app/api/prompt/route.js
@@ -2,21 +2,20 @@ import { connectToDB } from "@/utils/database";
 import Prompt from "@/models/prompt";
 
 export const GET = async (request) => {
-  const searchKeyword = request.nextUrl.searchParams.get("searchKeyword");
-  console.log("Search Keyword2:", searchKeyword);
+  //* const searchKeyword = request.nextUrl.searchParams.get("searchKeyword");
   try {
     await connectToDB();
 
-    //Query db with the filtered query
     let prompts = await Prompt.find().populate("creator");
 
-    // If a searchKeyword parameter is provided, filter prompts based on the keyword
-    if (searchKeyword) {
-      prompts = prompts.filter((prompt) =>
-        prompt.prompt.toLowerCase().includes(searchKeyword.toLowerCase())
-      );
-    }
+    //* If a searchKeyword parameter is provided, filter prompts based on the keyword
+    //* if (searchKeyword) {
+    //   prompts = prompts.filter((prompt) =>
+    //     prompt.prompt.toLowerCase().includes(searchKeyword.toLowerCase())
+    //   );
+    // }
 
+    //*This was done just for learning.
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all posts", { status: 500 });
--- promptify/components/Feed.jsx
+++ promptify/components/Feed.jsx
@@ -13,45 +13,63 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 const Feed = () => {
   const [posts, setPosts] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  let debounceTimer;
+  const [searchWords, setSearchWords] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  // let debounceTimer;
 
-  const handleSearchChange = async (e) => {
-    const query = e.target.value;
-    setSearchText(query);
+  const handleSearchChange = (e) => {
+    const query = e.target.value
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word.trim() !== ""); // Split on one or more whitespace characters and remove empty words
+    setSearchWords(e.target.value);
+
+    const filteredPosts = posts.filter((post) => {
+      return query.every((word) => {
+        const regex = new RegExp(word, "i");
+        return (
+          regex.test(post.prompt) ||
+          regex.test(post.tag) ||
+          regex.test(post.creator)
+        );
+      });
+    });
+
+    setFilteredPosts(filteredPosts);
   };
 
+  //Debouncing function made just for learning
   //Keyword searching with db fetching
-  useEffect(() => {
-    // Define a cleanup function to clear the debounce timer
-    const cleanup = () => {
-      clearTimeout(debounceTimer);
-    };
-    // Clear any previous debounce timer
-    clearTimeout(debounceTimer);
-    if (searchText != "") {
-      // Set a new debounce timer to wait 1 second
-      debounceTimer = setTimeout(async () => {
-        try {
-          const response = await fetch(
-            `/api/prompt?searchKeyword=${searchText}`
-          );
-          if (response.ok) {
-            const data = await response.json();
-            setPosts(data);
-          } else {
-            console.error("Error fetching search results:", response.status);
-          }
-        } catch (error) {
-          console.error("Error fetching search results:", error);
-        }
-      }, 1000);
-    } else {
-      fetchPosts();
-    }
-    // Return the cleanup function
-    return cleanup;
-  }, [searchText]);
+  // useEffect(() => {
+  //   // Define a cleanup function to clear the debounce timer
+  //   const cleanup = () => {
+  //     clearTimeout(debounceTimer);
+  //   };
+  //   // Clear any previous debounce timer
+  //   clearTimeout(debounceTimer);
+  //   if (searchText != "") {
+  //     // Set a new debounce timer to wait 1 second
+  //     debounceTimer = setTimeout(async () => {
+  //       try {
+  //         const response = await fetch(
+  //           `/api/prompt?searchKeyword=${searchText}`
+  //         );
+  //         if (response.ok) {
+  //           const data = await response.json();
+  //           setPosts(data);
+  //         } else {
+  //           console.error("Error fetching search results:", response.status);
+  //         }
+  //       } catch (error) {
+  //         console.error("Error fetching search results:", error);
+  //       }
+  //     }, 1000);
+  //   } else {
+  //     fetchPosts();
+  //   }
+  //   // Return the cleanup function
+  //   return cleanup;
+  // }, [searchText]);
 
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
@@ -59,23 +77,26 @@ const Feed = () => {
     setPosts(data);
   };
 
-  // useEffect(() => {
-
-  // }, []);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or a username..."
-          value={searchText}
+          placeholder="Search for prompt, tag or username..."
+          value={searchWords}
           onChange={handleSearchChange}
           required
           className="search_input peer"
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList
+        data={filteredPosts.length === 0 ? posts : filteredPosts}
+        handleTagClick={() => {}}
+      />
     </section>
   );
 };
